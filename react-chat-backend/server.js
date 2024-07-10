8226bd25332807f191c965475dcdee7b75ff0d2f
turan1302 <printwordex@gmail.com>
2024-07-10 17:54:05 +0300

    socket server kısmı kodlamaları gerçekleştirildi

--- react-chat-backend/server.js
+++ react-chat-backend/server.js
@@ -3,6 +3,8 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const PORT = 4444;
+
+const users = {};
 const privateUsers = {}; // anasayfadan kullanıcı bağlama
 
 io.on("connection",function (socket) {
@@ -15,8 +17,41 @@ io.on("connection",function (socket) {
 
         privateUsers[data.userId] = socket;
         socket.userId = data.userId;
-
     })
+
+    // kullanıcı mesajlaşma sayfasına girince çalışacak
+    socket.on("add_user",function (data){
+        if (!(data.sender_id in users)){
+            users[data.sender_id] = {};
+        }
+
+        users[data.sender_id][data.message_id] = socket;
+        socket.sender_id = data.sender_id;
+        socket.message_id = data.message_id;
+    });
+
+    // mesaj gönderilince çalışacak
+    socket.on("send_message",function (data){
+        if (data.receiver_id in users){
+            if (data.message_id in users[data.receiver_id]){
+                users[data.receiver_id][data.message_id].emit("message_show",{
+                    "mgc_sender" : data.sender_id,
+                    "mgc_content" : data.message,
+                })
+            }
+        }
+    });
+
+    // bildirim mesajı göndercez
+    socket.on("send_notification_message",function (data){
+       if (data.receiver_id in privateUsers){
+           privateUsers[data.receiver_id].emit("message_notification",{
+               "sender_id" : data.sender_id,
+               "dont_read" : data.dont_read
+           });
+       }
+    });
+
 
     socket.on("disconnect",function () {
         console.log("Birileri geldi ve gitti");
